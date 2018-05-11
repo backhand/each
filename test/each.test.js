344d5d1750562d9c25833899c658f4b9f0f17c0c
backhand <frederik@backhand.dk>
2018-05-11 13:08:06 +0200

    Multiple changes - Adds methods for setting each on global scope and installing   iterator on objects - ESLint config and linting - Version bump to 0.1.1

--- test/each.test.js
+++ test/each.test.js
@@ -11,7 +11,7 @@ describe('each', function() {
     };
 
     let i = 1;
-    for ([key, val] of each(obj)) {
+    for (const [key, val] of each(obj)) {
       assert.equal(key, `key${i}`);
       assert.equal(val, `val${i++}`);
     }
@@ -21,7 +21,7 @@ describe('each', function() {
     const arr = ['val0', 'val1', 'val2', 'val3', 'val4'];
 
     let i = 0;
-    for ([val, index] of each(arr)) {
+    for (const [val, index] of each(arr)) {
       assert.equal(val, `val${i}`);
       assert.equal(index, i++);
     }
@@ -32,7 +32,7 @@ describe('each', function() {
 
     const expected = str.split('');
     let i = 0;
-    for ([char, pos] of each(str)) {
+    for (const [char, pos] of each(str)) {
       assert.equal(char, expected[i]);
       assert.equal(pos, i++);
     }
@@ -40,5 +40,22 @@ describe('each', function() {
 
   it('should return an empty array if given unsupported type', () => {
     assert.deepEqual(each(123), []);
+  });
+
+  it('should iterate over keys and values of an object using Symbol.iterator on Object if enabled', () => {
+    each.setObjectsIterable();
+
+    const obj = {
+      key1: 'val1',
+      key2: 'val2',
+      key3: 'val3',
+      key4: 'val4'
+    };
+
+    let i = 1;
+    for (const [key, val] of obj) {
+      assert.equal(key, `key${i}`);
+      assert.equal(val, `val${i++}`);
+    }
   });
 }); // End describe testname
